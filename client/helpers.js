f386818ef204171189764ce9693be2c6d80a838c
Natalia Fierro <nataliabfierro@gmail.com>
2015-04-08 14:59:33 -0700

    creates book dummy data/update books/updates book template so it displays books selected in category

--- client/helpers.js
+++ client/helpers.js
@@ -1,6 +1,23 @@
-//helper function to retrieve categories from the database
+//helper function to retrieve categories from the database.
 Template.categories.helpers({
   Categories:function() {
     return Categories.find();
+  }
+});
+
+//States what catnotselected means. We basically need to say — look at category Session and if it’s null, the category isn’t selected yet
+Template.books.helpers({
+  catnotselected:function() {
+    return Session.equals("category",null);
+  },
+  category:function() {
+    return  Session.get('category');
+  }
+});
+
+//helper function to retrieve books from the database.
+Template.books.helpers({
+  booklist:function(){
+    return Books.find({catName:Session.get("category")});
   }
 });
