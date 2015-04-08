0ae14edbf420a8ab5fe9085e515b1ef5bb89eef7
Natalia Fierro <nataliabfierro@gmail.com>
2015-04-08 14:28:11 -0700

    basic routing/created layout, books, categories templates

---
+++ client/defaults.js
@@ -0,0 +1,2 @@
+//our switch for changing categories. Everytime we switch over the category, we want to show something else in category Session. Null means no category selected.
+Session.setDefault("category", null);
